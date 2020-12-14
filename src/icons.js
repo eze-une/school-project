d79de20cc2128b893fb2a296223d68f43354e8c8
Ezana Fitsum <mruniverse1x@gmail.com>
2020-12-14 12:20:02 +0300

    changed the icons.vue to .js and to the src directory

---
+++ src/icons.js
@@ -0,0 +1,7 @@
+import vue from 'vue'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons  '
+
+library.add(UserCircle)
+vue.component('fa-icon',fontAwesomeIcon)
